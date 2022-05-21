829066aef9edd6fb0d48625757ade5c23499e13e
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-21 12:42:42 +0300

    add views to create and show todos

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from email.policy import default
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import sys
 
 app = Flask(__name__)
 
@@ -19,4 +19,32 @@ class Todo(db.Model):
     completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
-        return f'<Todo: Id: {self.id}, name: {self.description}>'
+        return f'<Todo: Id: {self.id}, description: {self.description}>'
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():   
+   body={}
+   error = False
+   try: 
+       description =  request.get_json()['description']
+       todo = Todo(description=description)
+       body['description'] = todo.description
+       db.session.add(todo)
+       db.session.commit()
+   except:        
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+   finally:
+        db.session.close()           
+        if  error == True:
+            abort(400)
+        else:            
+            return jsonify(body)
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all())
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=3000)
