95836c35d4f290b29b757b0736160d7c27535525
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-29 20:36:12 +0300

    add foreignKey to todos schema and create todolist schema

--- app.py
+++ app.py
@@ -17,9 +17,16 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo: Id: {self.id}, description: {self.description}>'
+
+class TodoList(db.Model):
+    __tablename__ = "todolists"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    todos = db.relationShip('Todo', backref='todolists', lazy=True)
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():   
@@ -72,4 +79,5 @@ def index():
     return render_template('index.html', todos=Todo.query.order_by('id').all())
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", port=3000)
+    app.debug = True
+    app.run(host="0.0.0.0", port=3000)
